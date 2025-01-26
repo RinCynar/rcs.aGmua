ac1195acbaf076bae3046230426e4d94322dad11
RinCyanr <0rhendx@gmail.com>
2025-01-26 11:08:05 +0800

    1.91

--- scripts.js
+++ scripts.js
@@ -1,14 +1,13 @@
 document.addEventListener('DOMContentLoaded', () => {
     const updateLinks = document.querySelectorAll('a');
     
-    // 提示用户点击下载链接
     updateLinks.forEach(link => {
         link.addEventListener('mouseover', () => {
-            link.style.color = '#ff5722'; // 鼠标悬停时更改颜色
+            link.style.color = '#ff5722';
         });
         
         link.addEventListener('mouseout', () => {
-            link.style.color = ''; // 恢复默认颜色
+            link.style.color = ''; 
         });
     });
 });
